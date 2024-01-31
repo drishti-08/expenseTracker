ed48352725fe6c38486c3d16c2822506ae693489
drishti-08 <147835281+drishti-08@users.noreply.github.com>
2024-02-01 01:17:23 +0530

    Add files via upload

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Transaction> Transactions { get; set; }  //the name of the transaction table is transactions
+        public DbSet<Category> Categories { get; set; } // same as above
+    }
+}
